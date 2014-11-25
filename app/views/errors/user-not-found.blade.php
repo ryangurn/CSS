a1c20db9683d5917a635270ecf2691d21fee93f9
Ryan Gurnick <ryangurnick@gmail.com>
2014-11-25 15:10:51 -0500

    User not found view.

---
+++ app/views/errors/user-not-found.blade.php
@@ -0,0 +1,13 @@
+@section('content')
+<div class="col-sm-12">
+  <div class="chart-wrapper">
+    <div class="chart-title">
+      Student Not Found
+    </div>
+    <div class="chart-stage">
+
+    <p>If the information that you entered is correct than please contact the counseling office.</p>
+    </div>
+  </div>
+</div>
+@stop
