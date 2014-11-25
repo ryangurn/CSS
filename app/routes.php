6f24ef5e80cdca5dd4517a1ec118ee7b67604d36
Ryan Gurnick <ryangurnick@gmail.com>
2014-11-25 15:07:57 -0500

    Added Routes
    I added the routes for admin, logout, and home.

--- app/routes.php
+++ app/routes.php
@@ -12,8 +12,13 @@
 */
 Route::group(array('before' => 'guest'), function(){
   Route::get('/', array('as' => 'get-home', 'uses' => 'GetController@index'));
-});
-
-Route::group(array('before' => 'auth'), function(){
-
+  Route::post('/', array('as' => 'post-home', 'uses' => 'PostController@index'));
+  Route::get('/admin', array('as' => 'get-admin', 'uses' => 'AdminController@index'));
+  Route::get('/logout', function(){
+    Sentry::logout();
+    return Redirect::route('get-home');
+  });
+  Route::group(array('before' => 'csrf'), function(){
+    Route::post('/admin', array('as' => 'post-admin', 'uses' => 'AdminController@post_login'));
+  });
 });
